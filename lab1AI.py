f018792cfe6e912b3814ff7c2fe4d9f492bcca1e
KhadijaBCS22043 <khadijamustafa162@gmail.com>
2025-03-19 01:50:22 +0500

    Add files via upload

---
+++ lab1AI.py
@@ -0,0 +1,87 @@
+#Print String
+print("My Name Is Deeja." , "My age is 21.")
+
+#Comment
+# My basic Info
+print("My Name Is Deeja." , "My age is 21.") 
+
+
+#User input
+a=input("a:")
+print(a)
+
+#Multiple Lines on single line
+print("My Name Is Deeja." , "My age is 21.")
+
+#Indentation
+
+a=19
+if(a>=18):
+ print("you can go")
+
+#Data types
+a=123
+print(type(a))
+b=-9.8
+print(type(b))
+c=2.12e-10
+print(type(c))
+
+#Complex Number
+k=2+3j
+print(type(k))
+
+#Boolean
+x=True
+print(type(x))
+y=False
+print(type(y))
+
+# Strings
+b= "This is string with double quotes"
+c='This is String with single quote'
+
+#Special Characters
+print("It is the backslash (\\) Mark.")
+print("Its the tab \t key.")
+print("These are \"double quotes\"")
+print("Its \'single quote \'")
+print("This is new line \n new line")
+
+#String indices And accessing string elements
+str1=("Khadija Mustafa")
+print(str1[0])
+print(str1[-10])
+
+#String Slicing
+str1=("Khadija Mustafa")
+print(str1[3:7])
+
+#Using negative Indices
+str1 = ("Khadija Mustafa")
+print(str1[-6:-1])
+
+#Lists
+b=[1,2,3,4,5]
+c=["Deeja", "Noor", "Maryam"]
+a=[1,5,"Deeja",4]
+print(b,c,a)
+
+#Lists Slicing
+b=[1,2,3,4,5]
+c=["Deeja", "Noor", "Maryam"]
+print(b[1:3])
+
+#Negative Indices:
+b=[1,2,3,4,5]
+c=["Deeja", "Noor", "Maryam"]
+print(c[-3:-1])
+
+#Conditional Statements
+a=7
+b=14
+if b>a:
+ print("b is greater than a")
+
+
+
